d8a992f2ea7a97b064816b428084f3d5cc072c42
Lisa <liza.shoshkina@gmail.com>
2021-10-19 09:05:02 +0200

    Add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route} from 'react-router-dom';
 import Main from '../main/main';
+import Auth from '../auth/auth';
+import NotFoundPage from '../not-found-page/not-found-page';
+import MyList from '../my-list/my-list';
+import Film from '../film/film';
+import Player from '../player/player';
+import ReviewForm from '../review-form/review-form';
 
 const App = ({promoFilm}) => {
   return (
-    <Main
-      promoFilm={promoFilm}
-    />
+    <Switch>
+      <Route exact path="/">
+        <Main promoFilm={promoFilm} />
+      </Route>
+      <Route exact path="/login">
+        <Auth />
+      </Route>
+      <Route exact path="/mylist">
+        <MyList />
+      </Route>
+      <Route exact path="/films/:id">
+        <Film />
+      </Route>
+      <Route exact path="/films/:id/review">
+        <ReviewForm />
+      </Route>
+      <Route exact path="/player">
+        <Player />
+      </Route>
+      <Route>
+        <NotFoundPage />
+      </Route>
+    </Switch>
   );
 };
 
