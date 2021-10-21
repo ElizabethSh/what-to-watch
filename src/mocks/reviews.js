8e1b904b461583399818283d14c263a4f8c75330
Lisa <liza.shoshkina@gmail.com>
2021-10-21 17:53:28 +0200

    Add the reviews mocks

---
+++ src/mocks/reviews.js
@@ -0,0 +1,200 @@
+export const reviews = [
+  {
+    id: 1,
+    filmId: 1,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 8.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 2,
+    filmId: 1,
+    user: {
+      id: 5,
+      name: `Matthew Lickona`
+    },
+    rating: 7.9,
+    comment: `The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 3,
+    filmId: 2,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 7.2,
+    comment: `The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 4,
+    filmId: 2,
+    user: {
+      id: 6,
+      name: `Bill Goodykoontz`
+    },
+    rating: 7.5,
+    comment: `Anderson films are too precious for some, but for those of us willing to lose ourselves in them, they are a delight. The Grand Budapest Hotel is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 5,
+    filmId: 3,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 9.3,
+    comment: `I didnt find it amusing, and while I can appreciate the creativity, it is an hour and 40 minutes I wish I could take back.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 6,
+    filmId: 3,
+    user: {
+      id: 9,
+      name: `Paula Fleri-Soler`
+    },
+    rating: 7.7,
+    comment: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 6,
+    filmId: 3,
+    user: {
+      id: 9,
+      name: `Paula Fleri-Soler`
+    },
+    rating: 7.7,
+    comment: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 6,
+    filmId: 3,
+    user: {
+      id: 9,
+      name: `Paula Fleri-Soler`
+    },
+    rating: 7.7,
+    comment: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 6,
+    filmId: 3,
+    user: {
+      id: 9,
+      name: `Paula Fleri-Soler`
+    },
+    rating: 7.7,
+    comment: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 6,
+    filmId: 3,
+    user: {
+      id: 9,
+      name: `Paula Fleri-Soler`
+    },
+    rating: 7.7,
+    comment: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 7,
+    filmId: 4,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 8.2,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 8,
+    filmId: 5,
+    user: {
+      id: 10,
+      name: `Amanda Greever`
+    },
+    rating: 8.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 9,
+    filmId: 5,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 8.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 10,
+    filmId: 6,
+    user: {
+      id: 9,
+      name: `Paula Fleri-Soler`
+    },
+    rating: 7.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 11,
+    filmId: 6,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 8.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 12,
+    filmId: 7,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 7.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 13,
+    filmId: 7,
+    user: {
+      id: 4,
+      name: `Bill Goodykoontz`
+    },
+    rating: 9.0,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+  {
+    id: 14,
+    filmId: 7,
+    user: {
+      id: 4,
+      name: `Kate Muir`
+    },
+    rating: 8.9,
+    comment: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+    date: `2021-05-08T14:13:56.569Z`
+  },
+];
