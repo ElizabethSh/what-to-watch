00b488ab94a5db87ddb101bcbce1ec5c552d475e
Lisa <liza.shoshkina@gmail.com>
2021-10-21 17:50:35 +0200

    Add the FilmDetails component

---
+++ src/components/film-details/film-details.jsx
@@ -0,0 +1,65 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+import {filmProp} from '../../common/prop-types/film-props';
+
+const FilmDetails = ({film}) => {
+  const {
+    director,
+    starring,
+    genre,
+    released,
+    runTime
+  } = film;
+
+  return (
+    <div className="movie-card__text movie-card__row">
+      <div className="movie-card__text-col">
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Director</strong>
+          <span className="movie-card__details-value">{director}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Starring</strong>
+          <span className="movie-card__details-value">
+            {
+              starring.map((actor, index) => {
+                return (
+                  <Fragment key={actor}>
+                    {actor} {
+                      `${(index === starring.length - 1)
+                        ? ``
+                        : `,`}`
+                    }  <br/>
+                  </Fragment>
+                );
+              })
+            }
+          </span>
+        </p>
+      </div>
+
+      <div className="movie-card__text-col">
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Run Time</strong>
+
+          {/* 1h 39m */}
+          <span className="movie-card__details-value">{runTime}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Genre</strong>
+          <span className="movie-card__details-value">{genre}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Released</strong>
+          <span className="movie-card__details-value">{released}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+FilmDetails.propTypes = {
+  film: PropTypes.shape(filmProp),
+};
+
+export default FilmDetails;
