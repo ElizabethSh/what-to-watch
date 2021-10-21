2f0f8bf0be52dd5fe674baee55c7fb6d13d85fbe
Lisa <liza.shoshkina@gmail.com>
2021-10-21 17:49:50 +0200

    Add the FilmNav component

---
+++ src/components/film-nav/film-nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Tab} from '../../common/const';
+import {capitalizeString} from '../../common/utils';
+
+const Tabs = Object.values(Tab);
+
+const FilmNav = ({activeTab, onTabClick}) => {
+  return (
+    <nav className="movie-nav movie-card__nav">
+      <ul className="movie-nav__list">
+        {
+          Tabs.map((tab) => {
+            return (
+              <li key={tab}
+                className={`movie-nav__item
+                  ${tab === activeTab ? `movie-nav__item--active` : ``}
+                `}
+              >
+                <a href="#"
+                  className="movie-nav__link"
+                  data-tab={tab}
+                  onClick={(evt) => onTabClick(evt)}
+                >{capitalizeString(tab)}</a>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </nav>
+  );
+};
+
+FilmNav.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+  onTabClick: PropTypes.func.isRequired,
+};
+
+export default FilmNav;
