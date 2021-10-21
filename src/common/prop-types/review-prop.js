ce8d18e46094966af9e3e1de7d38923d59c8b34c
Lisa <liza.shoshkina@gmail.com>
2021-10-21 18:01:43 +0200

    Add the review prop-types

---
+++ src/common/prop-types/review-prop.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+export const reviewProp = {
+  id: PropTypes.number.isRequired,
+  filmId: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+  rating: PropTypes.number.isRequired,
+  comment: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
