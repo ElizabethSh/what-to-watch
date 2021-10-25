4a87bdc7dacd4447964f9ff0e74b531c6d5eb283
Lisa <liza.shoshkina@gmail.com>
2021-10-25 11:21:34 +0200

    Create a Redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import {createStore} from 'redux';
+import {devToolsEnhancer} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {films} from './mocks/films';
 import {reviews} from './mocks/reviews';
+import {reducer} from './store/reducer';
 
 const promoFilm = {
   name: `The Grand Budapest Hotel`,
@@ -12,13 +16,17 @@ const promoFilm = {
   released: 2014,
 };
 
+const store = createStore(reducer, devToolsEnhancer());
+
 ReactDom.render(
     <BrowserRouter>
-      <App
-        promoFilm={promoFilm}
-        films={films}
-        reviews={reviews}
-      />
+      <Provider store={store}>
+        <App
+          promoFilm={promoFilm}
+          films={films}
+          reviews={reviews}
+        />
+      </Provider>
     </BrowserRouter>,
     document.getElementById(`root`)
 );
