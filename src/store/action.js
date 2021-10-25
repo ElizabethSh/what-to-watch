f913c0d00c19ea497492a860b5b43dbb1fd67ef7
Lisa <liza.shoshkina@gmail.com>
2021-10-25 11:16:16 +0200

    Add the action for sorting films

---
+++ src/store/action.js
@@ -0,0 +1,15 @@
+import {ActionType} from './action-type';
+
+export const GenreAction = {
+  changeGenre: (genre) => {
+    return {
+      type: ActionType.CHANGE_GENRE,
+      payload: genre
+    };
+  },
+  resetGenre: () => {
+    return {
+      type: ActionType.RESET_GENRE
+    };
+  },
+};
