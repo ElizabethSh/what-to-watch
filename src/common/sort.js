8201a4285c9106766866f35c9a60fb7e0e1ae9d3
Lisa <liza.shoshkina@gmail.com>
2021-10-25 11:31:36 +0200

    Add the filmGenres array

--- src/common/sort.js
+++ src/common/sort.js
@@ -1,4 +1,8 @@
-import {DEFAULT_GENRE, filmGenres} from './const';
+import {DEFAULT_GENRE} from './const';
+import {getUniqueValues} from './utils';
+import {films} from '../mocks/films';
+
+export const filmGenres = getUniqueValues(films);
 
 export const sortItems = filmGenres.slice(0, 9);
 sortItems.unshift(DEFAULT_GENRE);
