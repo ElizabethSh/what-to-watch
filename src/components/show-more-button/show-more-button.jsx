bd86d366e3dd1ba4eae51a17ee50b3bda7762912
Lisa <liza.shoshkina@gmail.com>
2021-10-25 12:02:33 +0200

    Add the ShowMoreButton component

---
+++ src/components/show-more-button/show-more-button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ShowMoreButton = ({onShowMoreClick}) => {
+  return (
+    <div className="catalog__more">
+      <button
+        className="catalog__button"
+        type="button"
+        onClick={() => onShowMoreClick()}
+      >Show more</button>
+    </div>
+  );
+};
+
+ShowMoreButton.propTypes = {
+  onShowMoreClick: PropTypes.func.isRequired,
+};
+
+export default ShowMoreButton;
