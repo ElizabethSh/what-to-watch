e306326979c5da465a28d1a3e817197208e12ebd
Lisa <liza.shoshkina@gmail.com>
2021-10-27 14:35:44 +0200

    Update the storage initialization code, add thunk

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
-import {createStore} from 'redux';
-import {devToolsEnhancer} from 'redux-devtools-extension';
+import {applyMiddleware, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
-import {films} from './mocks/films';
 import {reviews} from './mocks/reviews';
-import {reducer} from './store/reducer';
+import {rootReducer} from './store/reducer/rootReducer';
+import {createApi} from './services/api';
 
 const promoFilm = {
   name: `The Grand Budapest Hotel`,
@@ -16,14 +17,17 @@ const promoFilm = {
   released: 2014,
 };
 
-const store = createStore(reducer, devToolsEnhancer());
+const api = createApi();
+
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument(api))
+));
 
 ReactDom.render(
     <BrowserRouter>
       <Provider store={store}>
         <App
           promoFilm={promoFilm}
-          films={films}
           reviews={reviews}
         />
       </Provider>
