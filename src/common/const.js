f6af92283376d083cbb9c9344a958657d8f6429a
Lisa <liza.shoshkina@gmail.com>
2021-10-28 11:51:25 +0200

    Add Enums: AppRoute, ApiRoute

--- src/common/const.js
+++ src/common/const.js
@@ -5,3 +5,16 @@ export const Tab = {
 };
 
 export const DEFAULT_GENRE = `All genres`;
+
+export const AppRoute = {
+  ROOT: `/`,
+  LOGIN: `/login`,
+  MY_LIST: `/mylist`,
+  FILMS: `/films`,
+  REVIEW: `/review`,
+  PLAYER: `/player`
+};
+
+export const ApiRoute = {
+  FILMS: `/films`,
+};
