b1afc419395086d8fffe389a179be1f3baade414
Lisa <liza.shoshkina@gmail.com>
2021-10-28 12:02:04 +0200

    Add the getFilmInfo action

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,8 +1,16 @@
+import {ApiRoute} from '../common/const';
 import {adaptFilmData} from '../services/adapter/film';
+import {FilmInfoAction} from './reducer/film-info/action';
 import {FilmsAction} from './reducer/films/action';
 
 export const getFilms = () => (dispatch, _getState, api) => {
-  return api.get(`/films`)
+  return api.get(ApiRoute.FILMS)
     .then(({data}) => data.map((it) => adaptFilmData(it)))
     .then((data) => dispatch(FilmsAction.loadFilms(data)));
+};
+
+export const getFilmInfo = (id) => (dispatch, _getState, api) => {
+  return api.get(`${ApiRoute.FILMS}/${id}`)
+    .then(({data}) => adaptFilmData(data))
+    .then((data) => dispatch(FilmInfoAction.getFilmInfo(data)));
 };
