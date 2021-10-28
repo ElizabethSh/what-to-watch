bbdd112d922e223afe7fa19745043f329e228396
Lisa <liza.shoshkina@gmail.com>
2021-10-28 12:12:43 +0200

    Add the FilmsAction

---
+++ src/store/reducer/films/action.js
@@ -0,0 +1,17 @@
+import {ActionType} from '../../action-type';
+
+export const FilmsAction = {
+  loadFilms: (films) => {
+    return {
+      type: ActionType.LOAD_FILMS,
+      payload: films
+    };
+  },
+
+  sortFilms: (genre) => {
+    return {
+      type: ActionType.SORT_FILMS,
+      payload: genre
+    };
+  }
+};
