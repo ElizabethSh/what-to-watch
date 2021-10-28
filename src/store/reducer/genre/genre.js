31bd9b693fcbf3912daecbaa1be2a53ae424c841
Lisa <liza.shoshkina@gmail.com>
2021-10-28 12:57:04 +0200

    Add the genre reducer

---
+++ src/store/reducer/genre/genre.js
@@ -0,0 +1,24 @@
+import {DEFAULT_GENRE} from '../../../common/const';
+import {ActionType} from '../../action-type';
+
+const initialState = {
+  activeGenre: DEFAULT_GENRE,
+};
+
+export const genre = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_GENRE:
+      return {
+        ...state,
+        activeGenre: action.payload,
+      };
+
+    case ActionType.RESET_GENRE:
+      return {
+        ...state,
+        activeGenre: initialState.activeGenre,
+      };
+  }
+
+  return state;
+};
