0367a78ec141d8944191f16903a24f1b3ed37fe2
Lisa <liza.shoshkina@gmail.com>
2021-10-28 11:56:51 +0200

    Add the sortFilms function

--- src/components/sort/sort.jsx
+++ src/components/sort/sort.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {GenreAction} from '../../store/action';
+import {GenreAction} from '../../store/reducer/genre/action';
 import {DEFAULT_GENRE} from '../../common/const';
+import {FilmsAction} from '../../store/reducer/films/action';
 
 const Sort = ({
   sortItems,
   activeGenre,
   changeActiveGenre,
   resetGenre,
+  sortFilms
 }) => {
   return (
     <ul className="catalog__genres-list">
@@ -24,6 +26,7 @@ const Sort = ({
                   resetGenre();
                 } else {
                   changeActiveGenre(sortItem);
+                  sortFilms(sortItem);
                 }
               }}
             >
@@ -40,13 +43,14 @@ const Sort = ({
 
 const mapStateToProps = (state) => {
   return {
-    activeGenre: state.activeGenre,
+    activeGenre: state.genre.activeGenre,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     changeActiveGenre: (genre) => dispatch(GenreAction.changeGenre(genre)),
+    sortFilms: (genre) => dispatch(FilmsAction.sortFilms(genre)),
     resetGenre: () => dispatch(GenreAction.resetGenre()),
   };
 };
@@ -56,6 +60,7 @@ Sort.propTypes = {
   activeGenre: PropTypes.string.isRequired,
   changeActiveGenre: PropTypes.func.isRequired,
   resetGenre: PropTypes.func.isRequired,
+  sortFilms: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sort);
