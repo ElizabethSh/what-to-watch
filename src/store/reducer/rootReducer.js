102d731717bb8e052b8cb93417dac5848985f70a
Lisa <liza.shoshkina@gmail.com>
2021-10-28 12:05:29 +0200

    Add the filmInfo reducer

--- src/store/reducer/rootReducer.js
+++ src/store/reducer/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {genre} from './genre/genre';
 import {films} from './films/films';
+import {filmInfo} from './film-info/film-info';
 
 export const rootReducer = combineReducers({
   genre,
-  films
+  films,
+  filmInfo,
 });
