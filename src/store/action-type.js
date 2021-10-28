384288437874ea7a2f8a20863af3228cd0fbb9ca
Lisa <liza.shoshkina@gmail.com>
2021-10-28 12:06:35 +0200

    Add the action types: LOAD_FILMS, SORT_FILMS, LOAD_FILM_INFO

--- src/store/action-type.js
+++ src/store/action-type.js
@@ -1,4 +1,10 @@
 export const ActionType = {
   CHANGE_GENRE: `genre/changeGenre`,
   RESET_GENRE: `genre/resetGenre`,
+
+  LOAD_FILMS: `films/loadFilms`,
+
+  SORT_FILMS: `sort/sortFilms`,
+
+  LOAD_FILM_INFO: `filmInfo/loadFilmInfo`,
 };
