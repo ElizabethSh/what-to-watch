fca926f7cfa40370126f50116f371949de1cbef9
Lisa <liza.shoshkina@gmail.com>
2021-10-28 11:55:49 +0200

    Add code to download films from the server

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {Switch, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 import Main from '../main/main';
 import Auth from '../auth/auth';
 import NotFoundPage from '../not-found-page/not-found-page';
 import MyList from '../my-list/my-list';
 import Film from '../film/film';
+import Loader from '../loader/loader';
 import Player from '../player/player';
 import AddReviewPage from '../add-review-page/add-review-page';
+import {getFilms} from '../../store/api-actions';
+import {AppRoute} from '../../common/const';
 import {filmProp} from '../../common/prop-types/film-props';
 import {reviewProp} from '../../common/prop-types/review-prop';
 
-const App = ({promoFilm, films, reviews}) => {
+
+const App = (props) => {
+  const {
+    promoFilm,
+    reviews,
+    films,
+    isFilmsLoaded,
+    uploadFilms
+  } = props;
+
+  useEffect(() => {
+    if (!isFilmsLoaded) {
+      uploadFilms();
+    }
+  }, [isFilmsLoaded]);
+
+  if (!isFilmsLoaded) {
+    return <Loader />;
+  }
+
   return (
     <Switch>
-      <Route exact path="/">
+      <Route exact path={AppRoute.ROOT}>
         <Main
           promoFilm={promoFilm}
-          films={films} />
+        />
       </Route>
-      <Route exact path="/login">
+      <Route exact path={AppRoute.LOGIN}>
         <Auth />
       </Route>
-      <Route exact path="/mylist">
+      <Route exact path={AppRoute.MY_LIST}>
         <MyList
           films={films}
         />
       </Route>
-      <Route exact path="/films/:id">
+      <Route exact path={`${AppRoute.FILMS}/:id`}>
         <Film
-          films={films}
           reviews={reviews}
         />
       </Route>
-      <Route exact path="/films/:id/review">
+      <Route exact path={`${AppRoute.FILMS}/:id${AppRoute.REVIEW}`}>
         <AddReviewPage
           film={films[2]}
         />
@@ -50,7 +72,22 @@ const App = ({promoFilm, films, reviews}) => {
   );
 };
 
+const mapStateToProps = (state) => {
+  return {
+    films: state.films.films,
+    isFilmsLoaded: state.films.isFilmsLoaded,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    uploadFilms: () => dispatch(getFilms())
+  };
+};
+
 App.propTypes = {
+  isFilmsLoaded: PropTypes.bool.isRequired,
+  uploadFilms: PropTypes.func.isRequired,
   promoFilm: PropTypes.shape({
     name: PropTypes.string.isRequired,
     posterImage: PropTypes.string.isRequired,
@@ -65,4 +102,4 @@ App.propTypes = {
   ).isRequired,
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
