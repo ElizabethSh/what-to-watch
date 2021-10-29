2a93737ee6bf092862095a3324ce7163b6b3b467
Lisa <liza.shoshkina@gmail.com>
2021-10-29 21:33:15 +0200

    Add interceptor to the createApi function

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,29 @@
 import axios from 'axios';
+import {StatusCode} from '../common/const';
 
-const baseURL = `https://6.react.pages.academy/wtw`;
-const timeout = 5000;
+const BASE_URL = `https://6.react.pages.academy/wtw`;
+const TIMEOUT = 5000;
 
-export const createApi = () => {
+export const createApi = (unAuthorised) => {
   const api = axios.create({
-    baseURL,
-    timeout,
+    baseURL: BASE_URL,
+    timeout: TIMEOUT,
     withCredentials: true
   });
+
+  const onSuccess = (response) => response;
+
+  const onError = (error) => {
+    const {response} = error;
+
+    if (response.statusCode === StatusCode.UNAUTHORIZED) {
+      unAuthorised();
+    }
+
+    throw error;
+  };
+
+  api.interceptors.response.use(onSuccess, onError);
 
   return api;
 };
