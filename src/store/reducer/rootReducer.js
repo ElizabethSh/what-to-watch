4e1072b8d1b462671cb6844046772e356ba400ab
Lisa <liza.shoshkina@gmail.com>
2021-11-10 15:38:03 +0100

    Add the NameSpace Enum

--- src/store/reducer/rootReducer.js
+++ src/store/reducer/rootReducer.js
@@ -7,12 +7,22 @@ import {reviews} from './reviews/reviews';
 import {favorites} from './favorites/favorites';
 import {user} from './user/user';
 
+export const NameSpace = {
+  GENRE: `GENRE`,
+  FILMS: `FILMS`,
+  FILM_INFO: `FILM_INFO`,
+  PROMO_FILM: `PROMO_FILM`,
+  REVIEWS: `REVIEWS`,
+  FAVORITES: `FAVORITES`,
+  USER: `USER`
+};
+
 export const rootReducer = combineReducers({
-  genre,
-  films,
-  filmInfo,
-  promoFilm,
-  reviews,
-  favorites,
-  user,
+  [NameSpace.GENRE]: genre,
+  [NameSpace.FILMS]: films,
+  [NameSpace.FILM_INFO]: filmInfo,
+  [NameSpace.PROMO_FILM]: promoFilm,
+  [NameSpace.REVIEWS]: reviews,
+  [NameSpace.FAVORITES]: favorites,
+  [NameSpace.USER]: user,
 });
