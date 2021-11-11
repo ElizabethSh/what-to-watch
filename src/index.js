50564754770d90594ac9e686b253fb649052699e
Lisa <liza.shoshkina@gmail.com>
2021-11-11 09:55:46 +0100

    Configure store

--- src/index.js
+++ src/index.js
@@ -2,26 +2,27 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
-import {applyMiddleware, createStore} from 'redux';
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {rootReducer} from './store/reducer/rootReducer';
 import {createApi} from './services/api';
 import {checkAuth} from './store/api-actions';
 import {setAuthStatus} from './store/reducer/user/action';
 import {AuthorizationStatus} from './common/const';
+import {configureStore} from '@reduxjs/toolkit';
 
 const api = createApi(
     () => store.dispatch(setAuthStatus(AuthorizationStatus.NO_AUTH))
 );
 
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(
-        applyMiddleware(thunk.withExtraArgument(api))
-    )
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      }
+    })
+});
 
 store.dispatch(checkAuth());
 
