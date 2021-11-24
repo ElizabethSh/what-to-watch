6645eaae7818bc4fb820aa07bc46cb2b8183c360
Lisa <liza.shoshkina@gmail.com>
2021-11-24 21:19:28 +0100

    Add code to play and pause video

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,32 +1,116 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
+import {useHistory, useParams} from 'react-router';
+import {formatToHoursMinutesSeconds, getArrayItem, toNumber} from '../../common/utils';
 import {filmProp} from '../../common/prop-types/film-props';
 
-const Player = ({film}) => {
-  const {runTime} = film;
+const Player = (props) => {
+  const {films} = props;
+
+  const [playerState, setPlayerState] = useState({
+    isPlaying: false,
+    progress: 0,
+  });
+
+  const history = useHistory();
+  const videoRef = useRef(null);
+
+  let {id} = useParams();
+  id = toNumber(id);
+
+  const {runTime, videoLink} = getArrayItem(films, id);
+
+  useEffect(() => {
+    if (playerState.isPlaying) {
+      videoRef.current.play();
+      return;
+    }
+
+    videoRef.current.pause();
+  }, [playerState.isPlaying]);
+
+  const exitClickHandler = () => {
+    history.goBack();
+  };
+
+  const onTimeUpdateHandler = () => {
+    const progress = (videoRef.current.currentTime / videoRef.current.duration) * 100;
+
+    setPlayerState({
+      ...playerState,
+      progress
+    });
+  };
+
+  const videoProgressHandler = (evt) => {
+    const manualChange = Number(evt.target.value);
+
+    videoRef.current.currentTime = (videoRef.current.duration / 100) * manualChange;
+
+    setPlayerState({
+      ...playerState,
+      progress: manualChange
+    });
+  };
 
   return (
     <div className="player">
-      <video src="#" className="player__video" poster="img/player-poster.jpg"></video>
+      <video
+        src={videoLink}
+        className="player__video"
+        poster="img/player-poster.jpg"
+        ref={videoRef}
+        onTimeUpdate={onTimeUpdateHandler}
+      />
 
-      <button type="button" className="player__exit">Exit</button>
+      <button type="button"
+        className="player__exit"
+        onClick={exitClickHandler}
+      >Exit</button>
 
       <div className="player__controls">
         <div className="player__controls-row">
           <div className="player__time">
-            {/* <progress className="player__progress" value="30" max="100"></progress> */}
-            {/* <div className="player__toggler" style="left: 30%;">Toggler</div> */}
+            <progress
+              className="player__progress"
+              value={playerState.progress}
+              max="100"
+            />
+            <div
+              className="player__toggler"
+              draggable
+              style={{left: `${playerState.progress}%`}}
+              onChange={(e) => videoProgressHandler(e)} // ?
+            >Toggler</div>
           </div>
-          {/* должно быть в формате 1:30:29 */}
-          <div className="player__time-value">{runTime}</div>
+          <div className="player__time-value">
+            {formatToHoursMinutesSeconds(runTime)}
+          </div>
         </div>
 
         <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
+          <button type="button"
+            className="player__play"
+            onClick={() => setPlayerState({
+              ...playerState,
+              isPlaying: !playerState.isPlaying
+            })}
+          >
+            {
+              playerState.isPlaying
+                ? <>
+                  <svg viewBox="0 0 14 21" width="14" height="21">
+                    <use xlinkHref="#pause"></use>
+                  </svg>
+                  <span>Pause</span>
+                </>
+                : <>
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </>
+            }
           </button>
           <div className="player__name">Transpotting</div>
 
@@ -44,7 +128,9 @@ const Player = ({film}) => {
 };
 
 Player.propTypes = {
-  film: PropTypes.shape(filmProp).isRequired,
+  films: PropTypes.arrayOf(
+      PropTypes.shape(filmProp)
+  ).isRequired,
 };
 
 export default Player;
