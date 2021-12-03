9ec4e51ea91fba75b76d82b8749ddaeb08764a48
Lisa <liza.shoshkina@gmail.com>
2021-12-03 11:42:18 +0100

    Add the VideoPlayer component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
 import {filmProp} from '../../common/prop-types/film-props';
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../common/const';
+import VideoPlayer from '../videoPlayer/videoPlayer';
 
-const Card = ({film, onMouseEnter}) => {
-  const {id, name, previewImage} = film;
+const Card = (props) => {
+  const {
+    film,
+    onMouseEnter,
+    onMouseLeave,
+    isPlaying
+  } = props;
+  const {
+    id,
+    name,
+    previewImage,
+    videoLink
+  } = film;
+
+  const videoRef = useRef(null);
 
   return (
     <article
       className="small-movie-card catalog__movies-card"
       onMouseEnter={() => onMouseEnter(id)}
+      onMouseLeave={onMouseLeave}
     >
       <Link className="small-movie-card__link"
         to={`${AppRoute.FILMS}/:${id}`}>
         <div className="small-movie-card__image">
-          <img
-            src={previewImage}
-            alt={name}
-            width="280" height="175"
+          <VideoPlayer
+            poster={previewImage}
+            src={videoLink}
+            videoRef={videoRef}
+            isMuted={true}
+            isPlaying={isPlaying}
           />
         </div>
         <h3 className="small-movie-card__title">
@@ -32,6 +49,8 @@ const Card = ({film, onMouseEnter}) => {
 Card.propTypes = {
   film: PropTypes.shape(filmProp).isRequired,
   onMouseEnter: PropTypes.func.isRequired,
+  onMouseLeave: PropTypes.func.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
 };
 
 export default Card;
