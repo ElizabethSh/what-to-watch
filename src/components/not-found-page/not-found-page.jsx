f5ba9a2c2362529da8968761bf3aac2fc8cbc75f
Lisa <liza.shoshkina@gmail.com>
2021-12-08 14:03:36 +0100

    Add the NotFoundPage component

---
+++ src/components/not-found-page/not-found-page.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../common/const';
+import './not-found-page.css';
+
+const NotFoundPage = () => {
+  return (
+    <div className="modal">
+      <div className="modal__backdrop"></div>
+      <section className="modal__window">
+        <h1 className="modal__title">Error 404: <br /> Page is not found</h1>
+        <p>
+          The page you are looking for might have been removed
+          had its name changed or is temporarily unavailable.
+        </p>
+        <Link className="modal__link catalog__button"
+          to={AppRoute.ROOT}
+        >Go to home page</Link>
+      </section>
+    </div>
+  );
+};
+
+export default NotFoundPage;
