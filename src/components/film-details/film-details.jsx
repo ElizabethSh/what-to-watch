42ef0a6b2de90061d906f2d5741301eeb46069ed
Lisa <liza.shoshkina@gmail.com>
2022-10-03 20:23:47 +0200

    Refactoring

--- src/components/film-details/film-details.jsx
+++ src/components/film-details/film-details.jsx
@@ -1,7 +1,7 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import {filmProp} from '../../common/prop-types/film-props';
-import {formatDuration} from '../../common/utils';
+import {formatToHoursMinutes} from '../../common/utils';
 
 const FilmDetails = ({film}) => {
   const {
@@ -42,7 +42,7 @@ const FilmDetails = ({film}) => {
       <div className="movie-card__text-col">
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Run Time</strong>
-          <span className="movie-card__details-value">{formatDuration(runTime)}</span>
+          <span className="movie-card__details-value">{formatToHoursMinutes(runTime)}</span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Genre</strong>
