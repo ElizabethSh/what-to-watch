8ac5b027f4ca7f16b5df3cc9d4b6eedc248cb6f7
Lisa Shoshkina <liza.shoshkina@gmail.com>
2025-09-15 16:37:11 +0200

    Chore/dependency updates (#18)

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
-import {StatusCode} from '../common/const';
+import axios from "axios";
+import { StatusCode } from "../common/const";
 
-const BASE_URL = `https://6.react.pages.academy/wtw`;
+const BASE_URL = `https://6.react.htmlacademy.pro/wtw`;
 const TIMEOUT = 5000;
 
 export const createApi = (unAuthorised) => {
   const api = axios.create({
     baseURL: BASE_URL,
     timeout: TIMEOUT,
-    withCredentials: true
+    withCredentials: true,
   });
 
   const onSuccess = (response) => response;
 
   const onError = (error) => {
-    const {response} = error;
+    const { response } = error;
 
-    if (response.status === StatusCode.UNAUTHORIZED) {
+    if (response && response.status === StatusCode.UNAUTHORIZED) {
       unAuthorised();
     }
 
